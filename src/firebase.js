ab1fa7a417a39fce3aed76067254e62bf7355548
Sayedrakhiya07 <Sayedrakhiya07@gmail.com>
2025-09-10 11:54:56 +0530

    google authentication added

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOgqsFMZVtQPTOXLVqW0OIKYSk6BLX-iU",
+  authDomain: "soul-buddy-c1174.firebaseapp.com",
+  projectId: "soul-buddy-c1174",
+  storageBucket: "soul-buddy-c1174.firebasestorage.app",
+  messagingSenderId: "720128674704",
+  appId: "1:720128674704:web:f97e6da256d1db7ace0115",
+  measurementId: "G-G099ZFCNYC"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
