b03b09ba699ea679d96c77e5111b6f3320466949
Sayedrakhiya07 <Sayedrakhiya07@gmail.com>
2025-09-12 14:52:43 +0530

    ui modified

--- src/components/LoginLogoutIcon.jsx
+++ src/components/LoginLogoutIcon.jsx
@@ -10,27 +10,45 @@ function LoginLogoutIcon() {
             setIsLoggedIn(false);
             sessionStorage.clear();
         } else {
-            setLoginModal(true);
+            setIsLoggedIn(true);
         }
     };
 
     return (
         <div
-            className="relative flex items-center justify-center w-[130px] h-[36px] rounded-3xl bg-[#474747]/30 border border-gray-500 cursor-pointer overflow-hidden"
+            className="relative flex items-center justify-center w-[113px] h-[41px]  cursor-pointer overflow-hidden "
+               style={{
+    background:"rgba(36, 33, 33, 1)",
+        border: "2px solid #474747",
+        borderRadius: "29px",
+        
+      }}
             onClick={handleClick}
         >
             {/* Sliding Circle (background only) */}
             <motion.div
                 initial={false}
-                animate={{ x: isLoggedIn ? 92 : 0 }}
+                animate={{ x: isLoggedIn ? 77 : 0 }}
                 transition={{ type: "spring", stiffness: 400, damping: 30 }}
-                className="absolute top-[3px] left-[3px] h-[30px] w-[30px] rounded-full bg-[#d9d9d9]/80"
+                className="absolute top-[5px] left-[3px] h-[28px] w-[28px] rounded-full bg-[#d9d9d9]/54 "
             />
 
             {/* Text always centered and above circle */}
-            <span className="z-10 text-sm font-semibold text-white">
-                {isLoggedIn ? "Logout" : "Login"}
-            </span>
+             <div className="flex items-center justify-center w-full z-10">
+             <span
+              style={{
+            fontSize: "18px",
+            fontWeight: 500,
+            fontFamily: "'Nunito', sans-serif",
+            color:"#7D7E7F"
+          }}
+          className={` ${
+            isLoggedIn ? "mr-6" : "ml-6"
+          }`}
+        >
+            {isLoggedIn ? "Logout" : "Login"}
+        </span>
+            </div>
         </div>
     );
 }
