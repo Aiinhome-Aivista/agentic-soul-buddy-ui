034f3aff84f06d1dd6aca659956664ff458f3136
Himanshu <294himangshu@gmail.com>
2026-01-06 11:19:17 +0530

    sign up button curser point

--- src/pages/AiChat.jsx
+++ src/pages/AiChat.jsx
@@ -117,11 +117,11 @@ const AiChat = () => {
     <div className="flex flex-col items-center w-[100%] h-[100%]">
 
 
-      <div className={`flex items-start justify-end gap-[1%] w-[100%]`}>
+      <div className={`flex items-start justify-end gap-[1%] w-[100%] `}>
         {!isLoggedIn && (
           <button
             onClick={() => setDisclaimerModal(true)}
-            className="h-[1.7rem] px-3 rounded-[1rem] border-2 border-[#333333] bg-[#474747]/22 text-[0.75rem] font-medium text-[#7D7E7F] hover:bg-[#474747]/40 transition-colors"
+            className="h-[1.7rem] px-3 rounded-[1rem] border-2 border-[#333333] bg-[#474747]/22 text-[0.75rem] font-medium text-[#7D7E7F] hover:bg-[#474747]/40 transition-colors cursor-pointer"
           >
             Sign Up
           </button>
