e4447795b7c43dd24f33cf8d90f7337ab44ca01f
Himanshu <294himangshu@gmail.com>
2026-01-08 18:35:37 +0530

    production ready

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss(),],
-  /* server: {
-    proxy: {
-      '/api': {
-        target: 'http://122.163.121.176:3029',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  }, */
+
 })
