5c5be6e04edc8a5bcce4fca2e9b9e07c9fea2aef
Pinki Paul <pp3785828@gmail.com>
2026-02-17 19:18:52 +0530

    added

--- src/pages/blog/components/BlogDetails.jsx
+++ src/pages/blog/components/BlogDetails.jsx
@@ -316,8 +316,8 @@ const BlogDetails = () => {
             <div className="w-full h-full bg-cover bg-center" style={{ backgroundImage: `url('${getImageUrl(blog.image_url || blog.featured_image || blog.image)}')` }}></div>
           </div>
         </section>
-        <div className="max-w-[1360px] mx-auto px-6 flex flex-col lg:flex-row justify-between gap-12 lg:gap-0">
-          <article className="lg:w-[75%] max-w-[950px] text-lg md:text-xl text-[#e5e7eb]/90 leading-relaxed [&_img]:rounded-2xl [&_img]:overflow-hidden lg:pr-8">
+        <div className="max-w-[1360px] mx-auto  flex flex-col lg:flex-row justify-between gap-12 lg:gap-0">
+          <article className="lg:w-[75%] max-w-[950px] text-lg md:text-xl text-[#e5e7eb]/90  [&_img]:rounded-2xl [&_img]:overflow-hidden lg:pr-8">
             {blog.content ? (
               <div className="prose prose-invert max-w-none">
                 {blog.content_preview && (
